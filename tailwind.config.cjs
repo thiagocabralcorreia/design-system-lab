3287415474d4c86db9d6229d1a4f31f5f788827a
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2022-10-11 20:37:52 -0300

    First commit: set tailwind global styles and storybook

---
+++ tailwind.config.cjs
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+
+      "gray-900": "#121214",
+      "gray-800": "#202024",
+      "gray-400": "#7C7C8A",
+      "gray-200": "#C4C4CC",
+      "gray-100": "#E1E1E6",
+
+      "cyan-500": "#61DAFB",
+      "gray-300": "#98E1FB",
+    },
+    extend: {
+      fontFamily: {
+        sans: "Inter, sans-serif",
+      },
+    },
+  },
+  plugins: [],
+};
